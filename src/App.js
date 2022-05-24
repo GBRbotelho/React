6b4568e7cc58f60cb07a8ec50be944f7db918e2c
GBRbotelho <gabrielbbotelho05@gmail.com>
2022-05-24 14:51:38 -0300

    Backend pronto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NewProject from "./components/pages/NewProject";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Container from "./components/Layout/Container";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Projects from "./components/pages/Projects";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-heigth">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
