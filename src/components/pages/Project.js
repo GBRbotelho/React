040b2087c5a49e1c5a1f90ac62d662e8b3440f65
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2022-06-05 19:43:16 -0300

    Botao Editar

---
+++ src/components/pages/Project.js
@@ -0,0 +1,72 @@
+import styles from "./Project.module.css";
+
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Loading from "../Layout/Loading";
+import Container from "../Layout/Container";
+
+function Project() {
+  const { id } = useParams();
+
+  const [project, setProject] = useState([]);
+  const [showProjectForm, setShowProjectForm] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log);
+    }, 200);
+  }, [id]);
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
+  return (
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {!showProjectForm ? "Editar Projeto" : "Fechar"}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria: </span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total de Orçamento: </span> R$ {project.budget}
+                  </p>
+                  <p>
+                    <span>Total utilizado: </span> R$ {project.costs}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <p>Detalhes do Projeto</p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
+}
+
+export default Project;
